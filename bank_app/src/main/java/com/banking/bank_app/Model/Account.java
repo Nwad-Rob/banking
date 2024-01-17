506db48ea74e03cfd975aeb967a6c46194e985e4
Nawd-Rob <rnwadinobi@gmail.com>
2024-01-16 19:45:50 -0600

    Updaed the POJO folder

--- bank_app/src/main/java/com/banking/bank_app/Model/Account.java
+++ bank_app/src/main/java/com/banking/bank_app/Model/Account.java
@@ -1,5 +1,70 @@
 package com.banking.bank_app.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Account")
 public class Account {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long AccountNumber;
+
+    @Column(name = "First_name")
+    private String firstname;
+
+    @Column(name = "Last_name")
+    private String lastName;
+
+    @Column(name = "Account_type")
+    private String AccountType;
+
+    @Column(name = "Status")
+    private String status;
+
+
+    public Long getAccountNumber() {
+        return this.AccountNumber;
+    }
+
+    public void setAccountNumber(Long AccountNumber) {
+        this.AccountNumber = AccountNumber;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccountType() {
+        return this.AccountType;
+    }
+
+    public void setAccountType(String AccountType) {
+        this.AccountType = AccountType;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
--- bank_app/src/main/java/com/banking/bank_app/Model/Admin.java
+++ bank_app/src/main/java/com/banking/bank_app/Model/Admin.java
@@ -1,5 +1,74 @@
 package com.banking.bank_app.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Adminstrators")
 public class Admin {
-    
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long adminId;
+
+    @Column(name ="First_name")
+    private String firstName;
+
+    @Column(name = "Last_name")
+    private String lastName;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+
+    public Long getAdminId() {
+        return this.adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
 }

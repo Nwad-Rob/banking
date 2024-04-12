f5d65cb7a7de0c6bc1ee3664ef50950576f837ae
Nwad_Rob <rnwadinobi@gmail.com>
2024-04-11 17:23:01 -0700

    changed the thymeleaf, create forms for thymeleaf to fully be operational

--- bank_app/src/main/java/com/banking/bank_app/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banking.bank_app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.banking.bank_app.Model.Transaction;
-
-public interface AdminRepository extends JpaRepository<Transaction, Long> {
-    
-}
